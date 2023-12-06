ff59f0d2d88166058b567c29592b3e19081fa399
manassk <skmanas2002@gmail.com>
2023-12-06 07:11:49 +0530

    Add more stylings and made HOME

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,8 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('Home') }}
                     </x-nav-link>
                 </div>
             </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,66 @@
 @extends('layouts.main')
 @section('contents')
-<h1>Posts</h1>
+<style>
+body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f0f2f5;
+}
+
+header {
+    background-color: #6DB9EF;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+    font-size:20px;
+}
+
+.content-feed {
+    max-width: 400px;
+    margin: 20px auto;
+    list-style: none;
+    display: grid;
+    gap: 20px;
+}
+
+.post-card {
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+}
+
+.card-image {
+    width: 100%;
+    height: auto;
+    border-bottom: 1px solid #ddd;
+}
+
+.card-title {
+    display: block;
+    margin: 10px;
+    text-decoration: none;
+    color: #385898;
+    font-weight: bold;
+}
+
+.card-title:hover {
+    background-color: #f0f0f0;
+}
+</style>
+<header>
+    <h1>Feed</h1>
+</header>
+
   <p><a href="{{route('posts.create')}}">Create a post</a></p>
-  <ul class = "courses-list ">
-    @foreach ($posts as $post)
-        <li><a href="{{route('posts.show' , [ 'id' => $post->id])}}">{{ $post->title }}</a></li>
-    @endforeach
-  </ul>
+  
+  <ul class="content-feed">
+        @foreach ($posts as $post)
+            <li class="post-card">
+                <img src="{{ asset('uploads/images/'.$post->post_image) }}" alt="image" class="card-image">
+                <a href="{{ route('posts.show', ['id' => $post->id]) }}" class="card-title">{{ $post->title }}</a>
+            </li>
+        @endforeach
+    </ul>
 @endsection
