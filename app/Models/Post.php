9edcbef81fb6575167f05d3297d04b050b5ce6e0
manassk <skmanas2002@gmail.com>
2023-12-07 18:55:17 +0530

    Set up comments model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
